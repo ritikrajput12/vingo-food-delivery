50311afd39cf4f1a56e520e2be8af7039b4f420e
Ritik <ritikrajput543678@gmail.com>
2026-04-17 00:03:41 +0530

    Fix: correct firebase import path in SignIn

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -10,7 +10,8 @@ import { ClipLoader } from 'react-spinners';
 import { useDispatch } from 'react-redux';
 import { setUserData } from '../redux/userSlice';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "../../firebase.js";
+
 
 
 function SignIn() {
