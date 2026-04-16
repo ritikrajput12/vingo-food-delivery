3bac19b8feab9e12417887dfbde3be651f3a2ee0
Ritik <ritikrajput543678@gmail.com>
2026-04-16 23:43:12 +0530

    Fix: correct Redux store import in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Provider stroe={store}>
+  <Provider store={store}>
     <App />
   </Provider>
   </BrowserRouter>
