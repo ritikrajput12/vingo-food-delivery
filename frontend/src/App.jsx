595b6bc060836353863053d6e80985e070d8522b
Ritik <ritikrajput543678@gmail.com>
2026-04-18 22:46:57 +0530

    Add: get user data from Redux in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import SignUp from './pages/signUp.jsx'
 import SignIn from './pages/signIn.jsx'
 import ForgotPassword from './pages/ForgotPassword.jsx'
 import useGetCurrentUser from './hooks/useGetCurrentUser.jsx'
+import { useSelector } from 'react-redux'
 
 export const serverUrl = "http://localhost:8000"
 
 function App() {
   useGetCurrentUser()
+  const {userData}=useSelector(state=>state.user)
   return (
     <Routes>
       <Route path='/signup' element={<SignUp/>}/>
