8c80f29345b23d4c0a2213e1635aec1fb7027390
Ritik <ritikrajput543678@gmail.com>
2026-04-19 22:27:44 +0530

    fixed routing logic in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import SignUp from './pages/signUp.jsx'
 import SignIn from './pages/signIn.jsx'
 import ForgotPassword from './pages/ForgotPassword.jsx'
 import useGetCurrentUser from './hooks/useGetCurrentUser.jsx'
 import { useSelector } from 'react-redux'
+import Home from './pages/Home.jsx'
 
 export const serverUrl = "http://localhost:8000"
 
@@ -12,10 +13,26 @@ function App() {
   useGetCurrentUser()
   const {userData}=useSelector(state=>state.user)
   return (
-    <Routes>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+     <Routes>
+      <Route
+        path="/signup"
+        element={!userData ? <SignUp /> : <Navigate to="/" />}
+      />
+
+      <Route
+        path="/signin"
+        element={!userData ? <SignIn /> : <Navigate to="/" />}
+      />
+
+      <Route
+        path="/forgot-password"
+        element={!userData ? <ForgotPassword /> : <Navigate to="/" />}
+      />
+
+      <Route
+        path="/"
+        element={userData ? <Home /> : <Navigate to="/signin" />}
+      />
     </Routes>
   )
 }
