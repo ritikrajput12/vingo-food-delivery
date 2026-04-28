73aa3b6dd313512972246c855662bf0f3928356a
Ritik <ritikrajput543678@gmail.com>
2026-04-28 21:51:35 +0530

    home update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import UserDashboard from '../components/userDashboard'
+import UserDashboard from '../components/UserDashboard'
 import OwnerDashboard from '../components/OwnerDashboard'
+import Nav from '../components/Nav'
 
 function Home() {
   const { userData } = useSelector(state => state.user)
 
   return (
     <div className='w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center bg-[#fff9f6]'>
-      {userData.role === "user" && <UserDashboard />}
-      {userData.role === "owner" && <OwnerDashboard />}
-      {userData.role === "deliveryBoy" && <UserDashboard />}
+
+      <Nav />
+
+      {userData && userData.role === "user" && <UserDashboard />}
+      {userData && userData.role === "owner" && <OwnerDashboard />}
+      {userData && userData.role === "deliveryBoy" && <UserDashboard />}
+
     </div>
   )
 }
