0c2e1bc80d46419fcc6da720cd4049b63e2cbc1b
Ritik <ritikrajput543678@gmail.com>
2026-04-30 17:41:09 +0530

    Added search icon in Nav component

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { FaLocationDot } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
 
 function Nav() {
   return (
     <div className='w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px] px-[20px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible'>
-      
+
       <h1 className='text-3xl font-bold mb-2 text-[#ff4d2d]'>
         Vingo
       </h1>
@@ -19,17 +20,15 @@ function Nav() {
           </div>
         </div>
 
-        {/* Search Input (YEH missing tha) */}
-        <div className='flex items-center w-[70%]'>
+        <div className='w-[80%] flex items-center gap-[10px]'>
+          <IoSearch size={25} className='text-[#ff4d2d]' />
           <input
             type="text"
             placeholder="search delicious food..."
-            className='w-full outline-none text-gray-600'
+            className='px-[10px] text-gray-700 outline-0 w-full'
           />
         </div>
-
       </div>
-
     </div>
   )
 }
